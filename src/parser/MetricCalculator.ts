22e4652dc87a21774a3f0d8437c4a80d81ee7e69
ResistantBear <bernhard.hoefler@hotmail.com>
2023-12-29 09:22:43 +0100

    More metric-related documentation

--- src/parser/MetricCalculator.ts
+++ src/parser/MetricCalculator.ts
@@ -10,10 +10,18 @@ import { Configuration } from "./Configuration";
 import { Metric, MetricResult, ParseFile } from "./metrics/Metric";
 import nodeTypesConfig from "./config/nodeTypesConfig.json";
 
+/**
+ * Arranges the calculation of all basic single file metrics on multiple files.
+ */
 export class MetricCalculator {
     private readonly fileMetrics: Metric[] = [];
     private config: Configuration;
 
+    /**
+     * Constructs a new instance of {@link MetricCalculator} for arranging the calculation of all
+     * basic single file metrics on multiple files.
+     * @param configuration Configuration that should be applied for this new instance.
+     */
     constructor(configuration: Configuration) {
         this.config = configuration;
         const allNodeTypes: ExpressionMetricMapping[] =
@@ -29,6 +37,11 @@ export class MetricCalculator {
         ];
     }
 
+    /**
+     * Calculates all basic single file metrics on the specified files.
+     * @param parseFiles Files for which the metrics should be calculated.
+     * @return Map that relates the file path of each file to a map of the calculated metrics.
+     */
     calculateMetrics(parseFiles: ParseFile[]) {
         const sourcesRoot = fs.realpathSync(this.config.sourcesPath);
 

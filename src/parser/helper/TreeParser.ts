d25d9365eb6a4d43aa7e587505516687cbb459c1
ResistantBear <34129499+ResistantBear@users.noreply.github.com>
2024-01-10 12:24:54 +0100

    Minor cleanup

--- src/parser/helper/TreeParser.ts
+++ src/parser/helper/TreeParser.ts
@@ -15,7 +15,6 @@ export class TreeParser {
         const parser = new Parser();
         parser.setLanguage(fileExtensionToGrammar.get(parseFile.fileExtension));
 
-        // As toString() without parameter uses UTF-8, using UTF-8 while reading the file doesn't change the result:
         const sourceCode = fs.readFileSync(parseFile.filePath, { encoding: "utf8" });
         const tree = parser.parse(sourceCode);
 

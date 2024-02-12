c7f5b9a61b00b6bb419ecb8da261d9134a6f7d7f
My Linh Dao <mylinh.dao@maibornwolff.de>
2024-02-12 11:41:43 +0100

    style: delete unused function declaration and simplify the code #97

--- src/parser/helper/ConstantTwoWayMap.ts
+++ src/parser/helper/ConstantTwoWayMap.ts
@@ -30,43 +30,18 @@ export class ConstantTwoWayMap<KeyType, ValueType> {
     }
 
     /**
-     * Retrieves all corresponding values for the keys of the specified iterable.
-     * Calls the passed function for all found values, skipping elements for which there is no value available in the map.
-     * @param iterable Iterable of elements to map.
-     * @param callback Function to call with each retrieved values.
-     */
-    retrieveValuesForAllKeys(iterable: Iterable<KeyType>, callback: (value: ValueType) => any) {
-        for (const key of iterable) {
-            const retrievedValue = this.getValueFor(key);
-            if (retrievedValue !== undefined) {
-                callback(retrievedValue);
-            }
-        }
-    }
-
-    /**
-     * Retrieves all corresponding keys for the values of the specified iterable. Calls the passed function for all
-     * found keys, skipping elements for which there is no key available in the map.
-     * @param iterable Iterable of values to reverse-map.
-     * @param callback Function to call with each retrieved key.
-     */
-    retrieveKeysForAllValues(iterable: Iterable<ValueType>, callback: (key: KeyType) => any) {
-        for (const value of iterable) {
-            const retrievedKey = this.getKeyFor(value);
-            if (retrievedKey !== undefined) {
-                callback(retrievedKey);
-            }
-        }
-    }
-
-    /**
      * Retrieves all corresponding keys for the values of the specified iterable.
      * Returns a set of all found keys.
      * @param iterable Iterable of values to reverse-map.
      */
     getKeysForAllValues(iterable: Iterable<ValueType>): Set<KeyType> {
         const set = new Set<KeyType>();
-        this.retrieveKeysForAllValues(iterable, (key) => set.add(key));
+        for (const value of iterable) {
+            const retrievedKey = this.getKeyFor(value);
+            if (retrievedKey !== undefined) {
+                set.add(retrievedKey);
+            }
+        }
         return set;
     }
 }

64427c36d276bd96498ea47074c72d8377483d51
My Linh Dao <mylinh.dao@maibornwolff.de>
2024-02-12 11:15:45 +0100

    test: resolve merge conflict #70

--- resources/java/functions/LambdaExpression.java
+++ resources/java/functions/LambdaExpression.java
@@ -4,22 +4,21 @@ public class SampleFunctions {
         int result = operation.performOperation(value);
         System.out.println("Operation Result: " + result);
     }
+
     public static int square(int num) {
         Function<Integer, Integer> squareFunction = n -> n * n;
         return squareFunction.apply(num);
     }
+
     public static void main(String[] args) {
         executeOperation(4, (x) -> x * 2); // Lambda expression for doubling the value
 
         int squaredResult = square(3);
         System.out.println("Square Result: " + squaredResult);
     }
+
     @FunctionalInterface
     interface MyFunctionalInterface {
         int performOperation(int value);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 442e08f (test: add deleted lambda test (Java) #70)

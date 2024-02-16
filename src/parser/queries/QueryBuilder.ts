77fd3107d0e6d0238aef920a4070e85990f1bbe7
ResistantBear <34129499+ResistantBear@users.noreply.github.com>
2024-02-16 09:35:27 +0100

    perf: first try for a tree sitter query cache #148

--- src/parser/queries/QueryBuilder.ts
+++ src/parser/queries/QueryBuilder.ts
@@ -7,14 +7,38 @@ let dlog: DebugLoggerFunction = debuglog("metric-gardener", (logger) => {
     dlog = logger;
 });
 
+const treeSitterQueryCache = new Map<any, Map<string, Query>>();
+
+/**
+ * Tries to retrieve the tree-sitter query from the cache. If there is no matching query,
+ * calls the constructor of tree-sitter.Parser.Query, which is a very performance-heavy operation.
+ */
+function retrieveTreeSitterQuery(treeSitterGrammar: any, queryString: string) {
+    let cachedQueriesForLanguage = treeSitterQueryCache.get(treeSitterGrammar);
+
+    if (cachedQueriesForLanguage !== undefined) {
+        const cachedQuery = cachedQueriesForLanguage.get(queryString);
+        if (cachedQuery !== undefined) {
+            return cachedQuery;
+        }
+    }
+    const newQuery = new Query(treeSitterGrammar, queryString);
+
+    if (cachedQueriesForLanguage === undefined) {
+        cachedQueriesForLanguage = new Map<string, Query>();
+        treeSitterQueryCache.set(treeSitterGrammar, cachedQueriesForLanguage);
+    }
+    treeSitterQueryCache.get(treeSitterGrammar)?.set(queryString, newQuery);
+}
+
 export class QueryBuilder {
     readonly #language: Language;
-    readonly #treeSitterLanguage: any;
+    readonly treeSitterGrammar: any;
     #statements: QueryStatementInterface[] = [];
 
     constructor(language: Language) {
         this.#language = language;
-        this.#treeSitterLanguage = languageToGrammar.get(language);
+        this.treeSitterGrammar = languageToGrammar.get(language);
     }
 
     setStatements(statements: QueryStatementInterface[]) {
@@ -36,14 +60,14 @@ export class QueryBuilder {
 
         if (this.#language !== Language.Unknown) {
             if (this.#statements.length > 0) {
-                queryString = this.getStatementsQuery();
+                queryString = this.#getStatementsQuery();
             }
 
             dlog("------------- Start Query: --------------");
             dlog(queryString);
             dlog("-----------------------------------------");
 
-            return new Query(this.#treeSitterLanguage, queryString);
+            return retrieveTreeSitterQuery(this.treeSitterGrammar, queryString);
         }
     }
 
@@ -54,7 +78,7 @@ export class QueryBuilder {
      * @return A query string.
      * @private
      */
-    private getStatementsQuery() {
+    #getStatementsQuery() {
         const statementQueries: string[] = [];
 
         for (const statement of this.#statements) {

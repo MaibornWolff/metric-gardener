1d5ad1f06313724a2504f9e39bcf46b50c3d3d82
Michael Alper Cezik <michaelalper.cezik@maibornwolff.de>
2024-02-22 16:41:00 +0100

    feat: added tests #143

--- test/metric-end-results/JSONMetrics.test.ts
+++ test/metric-end-results/JSONMetrics.test.ts
@@ -15,15 +15,23 @@ describe("JSON metrics tests", () => {
 
     describe("parses JSON nesting level metric", () => {
         it("should calculate nesting level", () => {
-            testFileMetric(jsonTestResourcesPath + "example.json", FileMetric.nestingLevel, 3);
+            testFileMetric(jsonTestResourcesPath + "example.json", FileMetric.nestingLevel, 2);
         });
 
         it("should also calculate nesting level", () => {
-            testFileMetric(jsonTestResourcesPath + "example2.json", FileMetric.nestingLevel, 4);
+            testFileMetric(jsonTestResourcesPath + "example2.json", FileMetric.nestingLevel, 3);
         });
 
         it("should correctly calculate the nesting level for a deep nested JSON file", () => {
-            testFileMetric(jsonTestResourcesPath + "deep.json", FileMetric.nestingLevel, 13);
+            testFileMetric(jsonTestResourcesPath + "deep.json", FileMetric.nestingLevel, 12);
+        });
+
+        it("should correctly calculate the nesting level for a unformatted JSON file", () => {
+            testFileMetric(jsonTestResourcesPath + "unformatted.json", FileMetric.nestingLevel, 3);
+        });
+
+        it("should correctly calculate the nesting level for an empty JSON file", () => {
+            testFileMetric(jsonTestResourcesPath + "empty.json", FileMetric.nestingLevel, 0);
         });
     });
 });

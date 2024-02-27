f8ac979932e26aa87163ef2e1e1aaef6ba3f3d00
ResistantBear <34129499+ResistantBear@users.noreply.github.com>
2024-02-27 13:02:10 +0100

    fix: fixed merge mistake #177

--- src/parser/helper/Language.ts
+++ src/parser/helper/Language.ts
@@ -29,12 +29,6 @@ export const enum Language {
     Ruby,
     Rust,
     Bash,
-    /**
-     * For files with unknown file extension. Could be a source code file written in language(s) for which
-     * no tree sitter grammar is installed, a missing entry in {@link fileExtensionToLanguage}
-     * or just a file that is no source code.
-     */
-    Unknown,
 }
 
 /**

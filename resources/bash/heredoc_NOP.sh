3e844c0b66d3c47e99c5ab4826d6a34e56edd521
My Linh Dao <mylinh.dao@maibornwolff.de>
2024-02-28 11:20:38 +0100

    fix: fix typing issue #177

--- resources/bash/heredoc_NOP.sh
+++ resources/bash/heredoc_NOP.sh
@@ -6,7 +6,7 @@ echo "Enter a number:"
 echo "Enter a number:" # This is an inline comment
 # comment
 : '
-This is a unofficial way to write multiline comment.
+This is an unofficial way to write multiline comment.
 It is not counted as comment, but as real code in our application.
 Its created by using the colon (:) as a no-op (NOP) command,
 followed by a block enclosed in single quotes.
@@ -16,7 +16,7 @@ c=2 #comment
 x=1
 
 : <<'END_COMMENT'
-This is also a unofficial way to write multiline comment.
+This is also an unofficial way to write multiline comment.
 It is not counted as comment, but as real code in our application.
 This is a heredoc (<<) redirected to a NOP command (:).
 The single quotes around END_COMMENT are important,

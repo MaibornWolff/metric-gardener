f48c5b8cf9e1077b57526595a2c869927cd67140
Clemens Pohle <clemens.pohle@maibornwolff.de>
2024-03-15 15:20:24 +0100

    test: add Perl complexity metric tests (need to be discussed) #190

--- test/metric-end-results/PerlMetrics.test.ts
+++ test/metric-end-results/PerlMetrics.test.ts
@@ -13,7 +13,74 @@ describe("Perl metrics tests", () => {
         results = await parseAllFileMetrics(path);
     });
 
-    // describe("parsing complexity metric", () => {});
+    describe("parsing complexity metric", () => {
+        it("should count compound statements correctly", () => {
+            // count: if, elsif, unless, when, while, until, for, foreach
+            // not: else, given, continue, {}
+            throw "unsure: BEGIN, UNITCHECK, CHECK, INIT, END";
+            testFileMetric(path + "compound-statements.pl", FileMetric.complexity, 23);
+        });
+
+        it("should ??? NOT ??? count defer blocks", () => {
+            throw "unsure: defer";
+            testFileMetric(path + "defer-blocks.pl", FileMetric.complexity, 0);
+        });
+
+        it("should ??? NOT ??? count dump", () => {
+            throw "unsure: dump, CORE::dump";
+            testFileMetric(path + "dump.pl", FileMetric.complexity, 0);
+        });
+
+        it("should ??? NOT ??? count exit", () => {
+            throw "unsure: exit";
+            testFileMetric(path + "exit.pl", FileMetric.complexity, 0);
+        });
+
+        it("should ??? NOT ??? count goto", () => {
+            throw "unsure: goto LABEL, goto EXPR, goto &NAME";
+            testFileMetric(path + "goto.pl", FileMetric.complexity, 0);
+        });
+
+        it("should ??? NOT ??? count loop control commands", () => {
+            // (count: while, if)
+            throw "unsure: next, last, redo";
+            testFileMetric(path + "loop-control.pl", FileMetric.complexity, 5);
+        });
+
+        it("should count operators correctly", () => {
+            // count: and, &&, or, ||, //, ?:
+            // not: everything else
+            throw "unsure: xor, &&=, ||=, //=";
+            testFileMetric(path + "operators.pl", FileMetric.complexity, 19);
+        });
+
+        it("should count statement modifiers correctly", () => {
+            // count: if, unless, while, until, for, foreach, when
+            // not: given
+            testFileMetric(path + "statement-modifiers.pl", FileMetric.complexity, 7);
+        });
+
+        it("should count subroutines", () => {
+            // count: sub definition, anonymous
+            // not: sub declaration
+            throw "unsure: AUTOLOAD";
+            testFileMetric(path + "sub.pl", FileMetric.complexity, 8);
+            testFileMetric(path + "sub-anonymous.pl", FileMetric.complexity, 6);
+            testFileMetric(path + "sub-autoload.pl", FileMetric.complexity, 1);
+        });
+
+        it("should count switch statements correctly", () => {
+            // count: when
+            // not: default
+            throw "unsure: for, foreach, given, continue, break";
+            testFileMetric(path + "switch-statements.pl", FileMetric.complexity, 10);
+        });
+
+        it("should count try-catch correctly", () => {
+            throw "unsure: eval, die, do, try, catch";
+            testFileMetric(path + "try-catch.pl", FileMetric.complexity, 0);
+        });
+    });
 
     // describe("parsing functions metric", () => {});
 

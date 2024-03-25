b696ea68233fe2388ecfc6e68110b0ce212f272d
My Linh Dao <mylinh.dao@maibornwolff.de>
2024-03-25 12:07:10 +0100

    docs: add Python doc #275

--- resources/python/case-statements.py
+++ resources/python/case-statements.py
@@ -1,11 +1,11 @@
 status = 403
-
+flag = False
 match status:
-    case 400:
+    case 400: #+1 complexity
         print("Bad request")
-    case 401 | 403 | 404:
+    case 401 | 403 | 404: #+1 complexity
         print("Not allowed")
-    case 418:
+    case 418 if flag: #+2 complexity ("if" is a guard)
         print("I'm a teapot")
     case _:
         print("Something's wrong with the internet")
--- resources/python/loops.py
+++ resources/python/loops.py
@@ -6,7 +6,18 @@ for item in list:
     for chara in item:
         print(chara)
 
+for item in list:
+    print(item)
+else:
+    print('hello')
+
 while True:
     for i in range(10):
         print(i)
     break
+
+num = 0
+while num < 3:
+    num+=1
+else:
+    print("end")

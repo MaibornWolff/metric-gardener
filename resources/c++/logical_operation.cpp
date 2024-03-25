2024e1e25713dafb36b484876175040847152714
Clemens Pohle <clemens.pohle@maibornwolff.de>
2024-03-25 08:39:58 +0100

    style: improve formatting #267

--- resources/c++/logical_operation.cpp
+++ resources/c++/logical_operation.cpp
@@ -3,6 +3,7 @@
 int main() {
     int x = 1;
     int y = 0;
+
     if (x > 0 and y > 0) {
         std::cout << "Both x and y are positive." << std::endl;
     }
@@ -22,9 +23,10 @@ int main() {
     if ((x > 0) ^ (y > 0)) {
         std::cout << "Exactly one of x and y is positive." << std::endl;
     }
+
     if ((x > 0) xor (y > 0)) {
-            std::cout << "Exactly one of x and y is positive." << std::endl;
-        }
+        std::cout << "Exactly one of x and y is positive." << std::endl;
+    }
 
     return 0;
 }
--- resources/php/logical-operations.php
+++ resources/php/logical-operations.php
@@ -3,4 +3,3 @@
 $result1 = true && false; // +1 complexity
 $result2 = true || false; // +1 complexity
 $result3 = true xor false; // +1 complexity
-

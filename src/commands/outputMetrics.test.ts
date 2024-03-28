5782671b18187e3bbd053b17d44fb50dc8b11693
My Linh Dao <mylinh.dao@maibornwolff.de>
2024-03-28 16:25:44 +0100

    fix: fix outputMetrics.test.ts after rebasing #176

--- src/commands/outputMetrics.test.ts
+++ src/commands/outputMetrics.test.ts
@@ -18,101 +18,101 @@ describe("outputMetrics", () => {
 
         afterAll(() => {
             vi.resetModules();
+        });
 
-            it("when metrics are present", () => {
-                const file1MetricResults: MetricResult[] = [];
-                file1MetricResults.push({ metricName: "metric1", metricValue: 42 });
-                file1MetricResults.push({ metricName: "metric2", metricValue: 43 });
+        it("when metrics are present", () => {
+            const file1MetricResults: MetricResult[] = [];
+            file1MetricResults.push({ metricName: "metric1", metricValue: 42 });
+            file1MetricResults.push({ metricName: "metric2", metricValue: 43 });
 
-                const file2MetricResults: MetricResult[] = [];
-                file2MetricResults.push({ metricName: "metric1", metricValue: 44 });
-                const file2MetricErrors: MetricError[] = [];
-                file2MetricErrors.push({ metricName: "metric2", error: new Error("Buh!") });
+            const file2MetricResults: MetricResult[] = [];
+            file2MetricResults.push({ metricName: "metric1", metricValue: 44 });
+            const file2MetricErrors: MetricError[] = [];
+            file2MetricErrors.push({ metricName: "metric2", error: new Error("Buh!") });
 
-                const fileMetrics = new Map([
-                    [
-                        "/file/path1.test",
-                        {
-                            fileType: FileType.SourceCode,
-                            metricResults: file1MetricResults,
-                            metricErrors: [],
-                        },
-                    ],
+            const fileMetrics = new Map([
+                [
+                    "/file/path1.test",
+                    {
+                        fileType: FileType.SourceCode,
+                        metricResults: file1MetricResults,
+                        metricErrors: [],
+                    },
+                ],
+                [
+                    "/file/path2.test",
+                    {
+                        fileType: FileType.SourceCode,
+                        metricResults: file2MetricResults,
+                        metricErrors: file2MetricErrors,
+                    },
+                ],
+            ]);
+
+            const unknownFiles = ["/file/path3.unknown", "/file/noExtension"];
+            const errorFiles = ["/file/path4.error"];
+
+            const relationShipMetrics: CouplingResult = {
+                relationships: [
+                    {
+                        fromNamespace: "fromNamespace",
+                        toNamespace: "toNamespace",
+                        usageType: "usage",
+                        fromSource: "/file/path2.test",
+                        toSource: "/file/path1.test",
+                        fromClassName: "ClassA",
+                        toClassName: "ClassB",
+                    },
+                ],
+                metrics: new Map([
                     [
                         "/file/path2.test",
                         {
-                            fileType: FileType.SourceCode,
-                            metricResults: file2MetricResults,
-                            metricErrors: file2MetricErrors,
+                            outgoing_dependencies: 3,
+                            incoming_dependencies: 2,
+                            instability: 0.6,
+                            coupling_between_objects: 2,
                         },
                     ],
-                ]);
+                ]),
+            };
 
-                const unknownFiles = ["/file/path3.unknown", "/file/noExtension"];
-                const errorFiles = ["/file/path4.error"];
+            outputAsJson(
+                fileMetrics,
+                unknownFiles,
+                errorFiles,
+                relationShipMetrics,
+                "mocked-file.json",
+                false,
+            );
 
-                const relationShipMetrics: CouplingResult = {
-                    relationships: [
-                        {
-                            fromNamespace: "fromNamespace",
-                            toNamespace: "toNamespace",
-                            usageType: "usage",
-                            fromSource: "/file/path2.test",
-                            toSource: "/file/path1.test",
-                            fromClassName: "ClassA",
-                            toClassName: "ClassB",
-                        },
-                    ],
-                    metrics: new Map([
-                        [
-                            "/file/path2.test",
-                            {
-                                outgoing_dependencies: 3,
-                                incoming_dependencies: 2,
-                                instability: 0.6,
-                                coupling_between_objects: 2,
-                            },
-                        ],
-                    ]),
-                };
+            expect(console.log).toHaveBeenCalledTimes(1);
+            expect(console.log).toHaveBeenCalledWith("Results saved to mocked-file.json");
 
-                outputAsJson(
-                    fileMetrics,
-                    unknownFiles,
-                    errorFiles,
-                    relationShipMetrics,
-                    "mocked-file.json",
-                    false,
-                );
+            expect(writeFileSync).toHaveBeenCalledTimes(1);
+            const [file, data, options] = writeFileSync.mock.lastCall!;
 
-                expect(console.log).toHaveBeenCalledTimes(1);
-                expect(console.log).toHaveBeenCalledWith("Results saved to mocked-file.json");
+            expect(file).toBe("mocked-file.json");
+            expect(data).toMatchSnapshot();
+            expect(options).toBeUndefined();
+        });
 
-                expect(writeFileSync).toHaveBeenCalledTimes(1);
-                const [file, data, options] = writeFileSync.mock.lastCall!;
+        it("when no metrics are present", () => {
+            const relationShipMetrics: CouplingResult = {
+                relationships: [],
+                metrics: new Map(),
+            };
 
-                expect(file).toBe("mocked-file.json");
-                expect(data).toMatchSnapshot();
-                expect(options).toBeUndefined();
-            });
+            outputAsJson(new Map(), [], [], relationShipMetrics, "mocked-file.json", false);
 
-            it("when no metrics are present", () => {
-                const relationShipMetrics: CouplingResult = {
-                    relationships: [],
-                    metrics: new Map()
-                };
+            expect(console.log).toHaveBeenCalledTimes(1);
+            expect(console.log).toHaveBeenCalledWith("Results saved to mocked-file.json");
 
-                outputAsJson(new Map(), [], [], relationShipMetrics, "mocked-file.json", false);
-
-                expect(console.log).toHaveBeenCalledTimes(1);
-                expect(console.log).toHaveBeenCalledWith("Results saved to mocked-file.json");
-
-                expect(writeFileSync).toHaveBeenCalledTimes(1);
-                expect(writeFileSync).toHaveBeenCalledWith(
-                    "mocked-file.json",
-                    '{"nodes":[],"info":[],"relationships":[]}',
-                );
-            });
+            expect(writeFileSync).toHaveBeenCalledTimes(1);
+            expect(writeFileSync).toHaveBeenCalledWith(
+                "mocked-file.json",
+                '{"nodes":[],"info":[],"relationships":[]}',
+            );
         });
     });
 });

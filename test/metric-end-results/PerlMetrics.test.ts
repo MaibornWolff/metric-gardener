2adbc5d382ffc353354924e828c139901696b71c
Clemens Pohle <clemens.pohle@maibornwolff.de>
2024-04-02 10:30:13 +0200

    fix: rebase issues #190

--- test/metric-end-results/PerlMetrics.test.ts
+++ test/metric-end-results/PerlMetrics.test.ts
@@ -1,16 +1,18 @@
-import { expectFileMetric, getCouplingMetrics, parseAllFileMetrics } from "./TestHelper";
-import { FileMetric, FileMetricResults } from "../../src/parser/metrics/Metric";
+import { expectFileMetric, getCouplingMetrics, parseAllFileMetrics } from "./TestHelper.js";
+import { FileMetric, FileMetricResults } from "../../src/parser/metrics/Metric.js";
+import { beforeAll, describe, expect, it } from "vitest";
 
 describe("Perl metrics tests", () => {
-    const path = "./resources/perl/";
+    const perlTestResourcesPath = "./resources/perl/";
 
     let results: Map<string, FileMetricResults>;
 
-    const testFileMetric = (inputPath: string, metric: FileMetric, expected: number) =>
-        expectFileMetric(results, inputPath, metric, expected);
+    function testFileMetric(path: string, metric: FileMetric, expected: number) {
+        expectFileMetric(results, perlTestResourcesPath + path, metric, expected);
+    }
 
     beforeAll(async () => {
-        results = await parseAllFileMetrics(path);
+        results = await parseAllFileMetrics(perlTestResourcesPath);
     });
 
     describe("parsing complexity metric", () => {
@@ -18,62 +20,62 @@ describe("Perl metrics tests", () => {
             // count: if, elsif, unless, when, while, until, for, foreach
             // as function: BEGIN, UNITCHECK, CHECK, INIT, END
             // not: else, given, continue, {}
-            testFileMetric(path + "compound-statements.pl", FileMetric.complexity, 28);
+            testFileMetric("compound-statements.pl", FileMetric.complexity, 28);
         });
 
         it("should NOT count dump", () => {
             // not: dump, CORE::dump
-            testFileMetric(path + "dump.pl", FileMetric.complexity, 0);
+            testFileMetric("dump.pl", FileMetric.complexity, 0);
         });
 
         it("should NOT count exit", () => {
             // not: exit
-            testFileMetric(path + "exit.pl", FileMetric.complexity, 0);
+            testFileMetric("exit.pl", FileMetric.complexity, 0);
         });
 
         it("should NOT count goto", () => {
             // (count: sub)
             // not: goto LABEL, goto EXPR, goto &NAME
-            testFileMetric(path + "goto.pl", FileMetric.complexity, 3);
+            testFileMetric("goto.pl", FileMetric.complexity, 3);
         });
 
         it("should NOT count loop control commands", () => {
             // (count: while, if)
             // not: next, last, redo
-            testFileMetric(path + "loop-control.pl", FileMetric.complexity, 5);
+            testFileMetric("loop-control.pl", FileMetric.complexity, 5);
         });
 
         it("should count operators correctly", () => {
             // count: and, &&, &&=, or, ||, ||=, //, //=, xor, ?:
             // not: everything else
-            testFileMetric(path + "operators.pl", FileMetric.complexity, 22);
+            testFileMetric("operators.pl", FileMetric.complexity, 22);
         });
 
         it("should count statement modifiers correctly", () => {
             // count: if, unless, while, until, for, foreach, when
             // not: given
-            testFileMetric(path + "statement-modifiers.pl", FileMetric.complexity, 7);
+            testFileMetric("statement-modifiers.pl", FileMetric.complexity, 7);
         });
 
         it("should count functions correctly", () => {
-            testFileMetric(path + "sub.pl", FileMetric.complexity, 8);
-            testFileMetric(path + "sub-anonymous.pl", FileMetric.complexity, 6);
-            testFileMetric(path + "sub-autoload.pl", FileMetric.complexity, 1);
-            testFileMetric(path + "classes.pl", FileMetric.complexity, 4);
-            testFileMetric(path + "classes-using-packages.pl", FileMetric.complexity, 4);
-            testFileMetric(path + "defer-blocks.pl", FileMetric.complexity, 1);
+            testFileMetric("sub.pl", FileMetric.complexity, 8);
+            testFileMetric("sub-anonymous.pl", FileMetric.complexity, 6);
+            testFileMetric("sub-autoload.pl", FileMetric.complexity, 1);
+            testFileMetric("classes.pl", FileMetric.complexity, 4);
+            testFileMetric("classes-using-packages.pl", FileMetric.complexity, 4);
+            testFileMetric("defer-blocks.pl", FileMetric.complexity, 1);
         });
 
         it("should count switch statements correctly", () => {
             // count: when, for, foreach
             // not: given, default, continue, break
-            testFileMetric(path + "switch-statements.pl", FileMetric.complexity, 12);
+            testFileMetric("switch-statements.pl", FileMetric.complexity, 12);
         });
 
         it("should count try-catch correctly", () => {
             // count: or, catch
             // not: eval, die, do, try
-            testFileMetric(path + "try-catch.pl", FileMetric.complexity, 2);
+            testFileMetric("try-catch.pl", FileMetric.complexity, 2);
         });
     });
 
@@ -81,34 +83,34 @@ describe("Perl metrics tests", () => {
         it("should count subroutines", () => {
             // count: sub definition
             // not: sub declaration
-            testFileMetric(path + "sub.pl", FileMetric.functions, 8);
+            testFileMetric("sub.pl", FileMetric.functions, 8);
         });
 
         it("should count anonymous subroutines", () => {
             // count: sub
-            testFileMetric(path + "sub-anonymous.pl", FileMetric.functions, 6);
+            testFileMetric("sub-anonymous.pl", FileMetric.functions, 6);
         });
 
         it("should count AUTOLOAD", () => {
             // count: AUTOLOAD
-            testFileMetric(path + "sub-autoload.pl", FileMetric.functions, 1);
+            testFileMetric("sub-autoload.pl", FileMetric.functions, 1);
         });
 
         it("should count class ADJUST and methods", () => {
             // count: ADJUST, method, sub, sub DESTROY
-            testFileMetric(path + "classes.pl", FileMetric.functions, 4);
-            testFileMetric(path + "classes-using-packages.pl", FileMetric.functions, 4);
+            testFileMetric("classes.pl", FileMetric.functions, 4);
+            testFileMetric("classes-using-packages.pl", FileMetric.functions, 4);
         });
 
         it("should count compound statements", () => {
             // count: BEGIN, UNITCHECK, CHECK, INIT, END
             // not: if, else, elsif, unless, given, when, while, until, for, foreach, continue, {}
-            testFileMetric(path + "compound-statements.pl", FileMetric.functions, 5);
+            testFileMetric("compound-statements.pl", FileMetric.functions, 5);
         });
 
         it("should count defer blocks", () => {
             // count: defer
-            testFileMetric(path + "defer-blocks.pl", FileMetric.functions, 1);
+            testFileMetric("defer-blocks.pl", FileMetric.functions, 1);
         });
     });
 
@@ -116,49 +118,49 @@ describe("Perl metrics tests", () => {
         it("should count classes correctly", () => {
             // count: class definition
             // not: class declaration
-            testFileMetric(path + "classes.pl", FileMetric.classes, 2);
+            testFileMetric("classes.pl", FileMetric.classes, 2);
         });
 
         it("should count classes using packages correctly", () => {
-            testFileMetric(path + "classes-using-packages.pl", FileMetric.classes, 2);
+            testFileMetric("classes-using-packages.pl", FileMetric.classes, 2);
         });
 
         it("should NOT count normal packages", () => {
-            testFileMetric(path + "modules/Module.pm", FileMetric.classes, 0);
+            testFileMetric("modules/Module.pm", FileMetric.classes, 0);
         });
     });
 
     describe("parsing lines_of_code metric", () => {
         it("should count comments correctly", () => {
-            testFileMetric(path + "comments.pl", FileMetric.linesOfCode, 11);
+            testFileMetric("comments.pl", FileMetric.linesOfCode, 11);
         });
 
         it("should count empty files correctly", () => {
-            testFileMetric(path + "empty.pl", FileMetric.linesOfCode, 1);
+            testFileMetric("empty.pl", FileMetric.linesOfCode, 1);
         });
 
         it("should count one line break correctly", () => {
-            testFileMetric(path + "line-break.pl", FileMetric.linesOfCode, 2);
+            testFileMetric("line-break.pl", FileMetric.linesOfCode, 2);
         });
 
         it("should count one line correctly", () => {
-            testFileMetric(path + "one-line.pl", FileMetric.linesOfCode, 1);
+            testFileMetric("one-line.pl", FileMetric.linesOfCode, 1);
         });
 
         it("should count pods correctly", () => {
-            testFileMetric(path + "pod.pl", FileMetric.linesOfCode, 27);
+            testFileMetric("pod.pl", FileMetric.linesOfCode, 27);
         });
 
         it("should count __DATA__ correctly", () => {
-            testFileMetric(path + "special-literals-data.pl", FileMetric.linesOfCode, 12);
+            testFileMetric("special-literals-data.pl", FileMetric.linesOfCode, 12);
         });
 
         it("should count __END__ correctly", () => {
-            testFileMetric(path + "special-literals.pl", FileMetric.linesOfCode, 20);
+            testFileMetric("special-literals.pl", FileMetric.linesOfCode, 20);
         });
 
         it("should count weird lines correctly", () => {
-            testFileMetric(path + "weird-lines.pl", FileMetric.linesOfCode, 15);
+            testFileMetric("weird-lines.pl", FileMetric.linesOfCode, 15);
         });
     });
 
@@ -167,57 +169,57 @@ describe("Perl metrics tests", () => {
             it("should count comments correctly", () => {
                 // count: any line with #
                 // not: # in strings
-                testFileMetric(path + "comments.pl", FileMetric.commentLines, 5);
+                testFileMetric("comments.pl", FileMetric.commentLines, 5);
             });
 
             it("should count pods", () => {
-                testFileMetric(path + "pod.pl", FileMetric.commentLines, 14);
+                testFileMetric("pod.pl", FileMetric.commentLines, 14);
             });
 
             it("should NOT count __DATA__", () => {
-                testFileMetric(path + "special-literals-data.pl", FileMetric.commentLines, 2);
+                testFileMetric("special-literals-data.pl", FileMetric.commentLines, 2);
             });
 
             it("should count __END__", () => {
                 // count: lines including and after __END__
-                testFileMetric(path + "special-literals.pl", FileMetric.commentLines, 4);
+                testFileMetric("special-literals.pl", FileMetric.commentLines, 4);
             });
         });
     });
 
     describe("parsing real_lines_of_code metric", () => {
         it("should NOT count comments", () => {
-            testFileMetric(path + "comments.pl", FileMetric.linesOfCode, 4);
+            testFileMetric("comments.pl", FileMetric.linesOfCode, 4);
         });
 
         it("should NOT count empty files", () => {
-            testFileMetric(path + "empty.pl", FileMetric.linesOfCode, 0);
+            testFileMetric("empty.pl", FileMetric.linesOfCode, 0);
         });
 
         it("should NOT count one line break", () => {
-            testFileMetric(path + "line-break.pl", FileMetric.linesOfCode, 0);
+            testFileMetric("line-break.pl", FileMetric.linesOfCode, 0);
         });
 
         it("should count one line correctly", () => {
-            testFileMetric(path + "line-break.pl", FileMetric.linesOfCode, 1);
+            testFileMetric("line-break.pl", FileMetric.linesOfCode, 1);
         });
 
         it("should NOT count pods", () => {
-            testFileMetric(path + "pod.pl", FileMetric.linesOfCode, 8);
+            testFileMetric("pod.pl", FileMetric.linesOfCode, 8);
         });
 
         it("should count __DATA__", () => {
-            testFileMetric(path + "special-literals-data.pl", FileMetric.linesOfCode, 4);
+            testFileMetric("special-literals-data.pl", FileMetric.linesOfCode, 4);
         });
 
         it("should NOT count after __END__", () => {
             // count: line with __END__
             // not: lines after __END__
-            testFileMetric(path + "special-literals.pl", FileMetric.linesOfCode, 12);
+            testFileMetric("special-literals.pl", FileMetric.linesOfCode, 12);
         });
 
         it("should count weird lines correctly", () => {
-            testFileMetric(path + "weird-lines.pl", FileMetric.linesOfCode, 10);
+            testFileMetric("weird-lines.pl", FileMetric.linesOfCode, 10);
         });
     });
 
@@ -225,7 +227,7 @@ describe("Perl metrics tests", () => {
 
     describe("parsing coupling metric", () => {
         it("should calculate correctly", async () => {
-            const couplingResult = await getCouplingMetrics(path);
+            const couplingResult = await getCouplingMetrics(perlTestResourcesPath);
             expect(couplingResult).toMatchSnapshot();
         });
     });

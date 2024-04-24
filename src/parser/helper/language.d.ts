c084161a8565287f0f8444418e54f2a0a4ba5f2c
Kevin Pham <hainamkevin.pham@gmail.com>
2024-04-24 17:19:28 +0200

    feat: add local declaration file for grammar #333

---
+++ src/parser/helper/language.d.ts
@@ -0,0 +1,15 @@
+declare module "tree-sitter-c-sharp";
+declare module "tree-sitter-cpp";
+declare module "tree-sitter-go";
+declare module "tree-sitter-java";
+declare module "tree-sitter-javascript";
+declare module "tree-sitter-kotlin";
+declare module "tree-sitter-php";
+declare module "tree-sitter-python";
+declare module "tree-sitter-typescript";
+declare module "tree-sitter-ruby";
+declare module "tree-sitter-rust";
+declare module "tree-sitter-bash";
+declare module "tree-sitter-c";
+declare module "tree-sitter-json";
+declare module "tree-sitter-yaml";

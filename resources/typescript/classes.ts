caea758ea99ddfe3ccda93fe6173c2e41a36d4cd
Kevin Pham <hainamkevin.pham@gmail.com>
2024-04-28 11:08:07 +0200

    fix: add function category to missing types #212

--- resources/typescript/classes.ts
+++ resources/typescript/classes.ts
@@ -3,3 +3,13 @@ class Test1 {}
 class Test2 {}
 
 class Test3 {}
+
+abstract class Animal {
+    constructor(protected name: string) { }
+
+    // abstract_method_signature
+    abstract makeSound(input : string) : string;
+
+    move(meters) {
+    }
+}

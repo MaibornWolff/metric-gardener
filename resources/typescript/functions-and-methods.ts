9a2d294f9a507f895ed4982aa27c121a43bd9c5d
Kevin Pham <hainamkevin.pham@gmail.com>
2024-04-28 11:08:07 +0200

    fix: add function category to missing types #212

--- resources/typescript/functions-and-methods.ts
+++ resources/typescript/functions-and-methods.ts
@@ -27,3 +27,9 @@ const myFunction = function myFunctionDefinition() {
 };
 
 const x = (x, y) => x * y;
+
+// method_signature
+interface KeyValueProcessor{ functionName(key: number, value: string): void; }
+
+// call_signature
+interface KeyValueProcessor{ (key: number, value: string): void; };

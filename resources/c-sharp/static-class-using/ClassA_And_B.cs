88f23b200c57c3f710d32897a5f3f21defbb0d4b
Kevin Pham <hainamkevin.pham@gmail.com>
2024-05-17 12:00:21 +0200

    test: add static class usage example #356

---
+++ resources/c-sharp/static-class-using/ClassA_And_B.cs
@@ -0,0 +1,56 @@
+using static System.Console;
+
+namespace StaticUsedNamespace
+{
+    public static class ClassA
+    {
+        public void MyMethod()
+        {
+            WriteLine("Hello from ClassA's MyMethod!");
+        }
+
+    }
+
+    public static class ClassB
+    {
+        public void MyMethod()
+        {
+            WriteLine("Hello from ClassA's MyMethod!");
+        }
+    }
+}
+
+namespace StaticUsedNamespace2
+{
+
+    public class ClassA
+    {
+        public static void MyMethod2()
+        {
+            test();
+        }
+
+        void test()
+        {
+        }
+    }
+}
+
+namespace NormalUsedNamespace
+{
+    public class ClassB
+    {
+        public static void MyMethod()
+        {
+            WriteLine("Hello from ClassA's MyMethod!");
+        }
+    }
+
+    public class ClassC
+    {
+        public static void MyMethod()
+        {
+            WriteLine("Hello from ClassA's MyMethod!");
+        }
+    }
+}

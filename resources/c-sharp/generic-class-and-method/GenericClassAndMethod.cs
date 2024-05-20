961a8ddefa39e75da0d1f17f0b222f5935346f95
MyLinhD <mylinh.dao@t-online.de>
2024-05-20 08:38:57 +0200

    test: add generic test resources #356

---
+++ resources/c-sharp/generic-class-and-method/GenericClassAndMethod.cs
@@ -0,0 +1,19 @@
+namespace GenericNamespace
+{
+    public class GenericClass<S>
+    {
+    }
+    public class ClassWithGenericMethod
+    {
+        public static void genericMethod<TypeOfValue>(){
+        }
+    }
+    public class InputTypeForGenericClass
+    {
+
+    }
+    public class InputTypeForGenericMethod
+    {
+
+    }
+}

d4cadf20f72f3687b9960ebad709396d09d7cfa9
Benedikt Mehl <benedikt.mehl@maibornwolff.de>
2024-05-28 14:41:40 +0200

    test: add more tests for types-query-strategy.ts #366

--- src/parser/resolver/types/resolver-strategy/types-query-strategy.test.ts
+++ src/parser/resolver/types/resolver-strategy/types-query-strategy.test.ts
@@ -5,6 +5,7 @@ import { Configuration } from "../../../configuration.js";
 import { type ParsedFile } from "../../../metrics/metric.js";
 import { CSharpCollector } from "../c-sharp-collector.js";
 import { type AbstractCollector, type TypeInfo } from "../abstract-collector.js";
+import { PHPCollector } from "../php-collector.js";
 import { TypesQueryStrategy } from "./types-query-strategy.js";
 
 async function getConfiguration(filePath: string): Promise<Configuration> {
@@ -23,8 +24,6 @@ async function getConfiguration(filePath: string): Promise<Configuration> {
 describe("Types Query strategy", () => {
     describe("function getTypesFromFile()", () => {
         it("should calculate types declarations for interfaces", async () => {
-            /* Since we add the "node" field to TypeInfo, we have to mock that node to run this test.
-            This makes the test very complicated...
             // Given
             const filePath = "resources/c-sharp/relation-between-interfaces-in-one-file/Program.cs";
             const parsedFile: ParsedFile = (await parse(
@@ -33,7 +32,7 @@ describe("Types Query strategy", () => {
             )) as ParsedFile;
             const csharpCollector: AbstractCollector = new CSharpCollector();
 
-            const result: Map<string, TypeInfo> = new Map<string, TypeInfo>();
+            const result: Map<FQTN, TypeInfo> = new Map<string, TypeInfo>();
 
             result.set("mainNamespace.FirstInterface", {
                 namespace: "mainNamespace",
@@ -60,13 +59,119 @@ describe("Types Query strategy", () => {
                 implementedFrom: [],
             });
             // When
+            const typesFromFile: Map<FQTN, TypeInfo> = new TypesQueryStrategy().getTypesFromFile(
+                parsedFile,
+                ".",
+                csharpCollector.getTypesQuery(),
+            );
+            // Then
+            for (const [key, value] of typesFromFile.entries()) {
+                expect(value).toEqual(expect.objectContaining(result.get(key)));
+            }
+        });
+
+        it("should calculate correct types from multiple namespaces within one file for c-sharp", async () => {
+            // Given
+            const filePath = "resources/c-sharp/coupling-examples/BlubController.cs";
+            const parsedFile: ParsedFile = (await parse(
+                filePath,
+                await getConfiguration(filePath),
+            )) as ParsedFile;
+            const csharpCollector: AbstractCollector = new CSharpCollector();
+
+            const result: Map<FQTN, TypeInfo> = new Map<string, TypeInfo>();
+
+            result.set("App.CouplingExamplesOne.BlubControllerOne1", {
+                namespace: "App.CouplingExamplesOne",
+                typeName: "BlubControllerOne1",
+                classType: "class",
+                sourceFile: filePath,
+                namespaceDelimiter: ".",
+                implementedFrom: [],
+            });
+            result.set("App.CouplingExamplesOne.BlubControllerOne2", {
+                namespace: "App.CouplingExamplesOne",
+                typeName: "BlubControllerOne2",
+                classType: "class",
+                sourceFile: filePath,
+                namespaceDelimiter: ".",
+                implementedFrom: [],
+            });
+            result.set("App.CouplingExamplesTwo.BlubControllerTwo1", {
+                namespace: "App.CouplingExamplesTwo",
+                typeName: "BlubControllerTwo1",
+                classType: "class",
+                sourceFile: filePath,
+                namespaceDelimiter: ".",
+                implementedFrom: [],
+            });
+            result.set("App.CouplingExamplesTwo.BlubControllerTwo2", {
+                namespace: "App.CouplingExamplesTwo",
+                typeName: "BlubControllerTwo2",
+                classType: "class",
+                sourceFile: filePath,
+                namespaceDelimiter: ".",
+                implementedFrom: [],
+            });
+            // When
             const typesFromFile = new TypesQueryStrategy().getTypesFromFile(
                 parsedFile,
                 ".",
                 csharpCollector.getTypesQuery(),
             );
             // Then
-            expect(typesFromFile).toStrictEqual(result); */
+            for (const [key, value] of typesFromFile.entries()) {
+                expect(value).toEqual(expect.objectContaining(result.get(key)));
+            }
+        });
+
+        it("should calculate correct types from multiple namespaces within one file for php", async () => {
+            // Given
+            const filePath = "resources/php/coupling-examples/BlubController.php";
+            const parsedFile: ParsedFile = (await parse(
+                filePath,
+                await getConfiguration(filePath),
+            )) as ParsedFile;
+            const phpCollector: PHPCollector = new PHPCollector();
+
+            const result: Map<FQTN, TypeInfo> = new Map<string, TypeInfo>();
+
+            result.set("App\\CouplingExamplesOne\\BlubControllerOne1", {
+                namespace: "App\\CouplingExamplesOne",
+                typeName: "BlubControllerOne1",
+                classType: "class",
+                sourceFile: filePath,
+                namespaceDelimiter: "\\",
+                implementedFrom: ["ControllerInterface"],
+            });
+            result.set("App\\CouplingExamplesOne\\BlubControllerOne2", {
+                namespace: "App\\CouplingExamplesOne",
+                typeName: "BlubControllerOne2",
+                classType: "class",
+                sourceFile: filePath,
+                namespaceDelimiter: "\\",
+                implementedFrom: [],
+            });
+            result.set("App\\CouplingExamplesTwo\\BlubControllerTwo1", {
+                namespace: "App\\CouplingExamplesTwo",
+                typeName: "BlubControllerTwo1",
+                classType: "class",
+                sourceFile: filePath,
+                namespaceDelimiter: "\\",
+                extendedFrom: "BlubControllerOne1",
+                implementedFrom: [],
+            });
+
+            // When
+            const typesFromFile = new TypesQueryStrategy().getTypesFromFile(
+                parsedFile,
+                "\\",
+                phpCollector.getTypesQuery(),
+            );
+            // Then
+            for (const [key, value] of typesFromFile.entries()) {
+                expect(value).toEqual(expect.objectContaining(result.get(key)));
+            }
         });
     });
 });

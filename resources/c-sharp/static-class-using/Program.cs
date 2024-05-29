baa9ddd464c0cf4e19dd8135ab87f6fbe50c2ede
BenediktMehl <b.mehl14@outlook.de>
2024-05-29 18:35:03 +0200

    test: fix static class using test  #372

---
+++ resources/c-sharp/static-class-using/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using StaticUsedNamespace;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(ClassA.MyMethod());
+        }
+    }
+}
+

a1ac770cc03da6ac7798b07d0bcbce2c9e6ef75c
BenediktMehl <b.mehl14@outlook.de>
2024-05-31 12:45:46 +0200

    refactor: fix some lint warnings #372

--- src/parser/queries/query-builder.ts
+++ src/parser/queries/query-builder.ts
@@ -1,7 +1,7 @@
 import { debuglog, type DebugLoggerFunction } from "node:util";
 import { Query } from "tree-sitter";
 import { type Language, languageToGrammar } from "../../helper/language.js";
-import { type QueryStatement, SimpleLanguageSpecificQueryStatement } from "./query-statements.js";
+import { type QueryStatement } from "./query-statements.js";
 
 let dlog: DebugLoggerFunction = debuglog("metric-gardener", (logger) => {
     dlog = logger;
